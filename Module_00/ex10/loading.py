6d3c6c3ffa7f2e3f86a7d4cbca74daa3d727331d
Lea Lescure <lea.lescure@hotmail.fr>
2021-11-09 23:17:16 +0100

    ex10 over norm to do

--- Module_00/ex10/loading.py
+++ Module_00/ex10/loading.py
@@ -1,13 +1,29 @@
 import time
 import sys
 
-def ft_progress(lst):
-    n = 0
+def progress_bar(number):
+    bar_lenght = 15
+    filled_lenght = int(15 * number / 1000)
+    bar = '=' * filled_lenght + '>'
+    sys.stdout.write("[%s]" % bar)
+
+
+def ft_progress(listy):
+    number = 0
     start = time.time()
-    for n in lst:
-        n += 1
-        yield n
-        print(n)
+    for number in listy:
+        number += 1
+        yield number
+        end = time.time()
+        elapsed_time = end - start
+        percentage = int(number / 10)
+        eta = (1000 - number) / 100
+        sys.stdout.write("ETA: %.2fs " % eta)
+        sys.stdout.write("[ %d%%]" % percentage)
+        progress_bar(number)
+        sys.stdout.write(" %d/1000 | " % number)
+        sys.stdout.write("elapsed time %.2fs\r" % elapsed_time)
+
 
 listy = range(1000)
 ret = 0
