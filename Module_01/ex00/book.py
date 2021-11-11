63568a7f705e6599a32c7a6f9bcfcaaac46ec58c
Lea Lescure <lea.lescure@hotmail.fr>
2021-11-11 15:29:09 +0100

    ex00 only works when you add one recipe for each recipe_type

--- Module_01/ex00/book.py
+++ Module_01/ex00/book.py
@@ -1,8 +1,6 @@
 from datetime import date
 from recipe import Recipe
 
-meals = ["starter", "lunch", "dessert"]
-
 class Book:
     def __init__(self, name):
         try:
@@ -10,32 +8,32 @@ class Book:
         except:
             print("Name is not a string")
             exit()
-        self.last_update = date.today()
         self.creation_date = date.today()
+        self.last_update = self.creation_date
+        self.recipes_list = {"starter": None, "lunch": None, "dessert": None}
 
     def get_recipe_by_name(self, name):
         """Prints a recipe with the name textt{name} and returns the instance"""
-        for iterator_meals in meals:
-            for iterator_recipes in recipes_list[meals]:
-                if name == iterator_recipes:
-                    print(str(name))
-            else:
-                print("This recipe doesn't exist")
+        for key, value in self.recipes_list.items():
+            if value != None:
+                if value.name == name:
+                    print(value)
+                else:
+                    print("This recipe does not exist")
         pass
 
     def get_recipes_by_types(self, recipe_type):
         """Get all recipe names for a given recipe_type """
-        for iterator_meals in meals:
-            if iterator_meals == recipe_type:
-                for iterator_recipes in recipes_list[iterator_meals]:
-                    print(str(recipes_list[iterator_meals][iterator_recipes]))
+        for key, value in self.recipes_list.items():
+            if key == recipe_type:
+                print(value)
 
         pass
 
     def add_recipe(self, recipe):
         """Add a recipe to the book and update last_update"""
-        for iterator_meals in meals:
-            if iterator_meals == recipe.recipe_type:
-               recipes_list[iterator_meals].append(recipe)
+        for key, value in self.recipes_list.items():
+            if key == recipe.recipe_type:
+               self.recipes_list[key] = recipe
         last_update = date.today()
         pass
--- Module_01/ex00/test.py
+++ Module_01/ex00/test.py
@@ -5,12 +5,24 @@ porridge = Recipe("porridge", 2, 5, ["oat", "milk", "honey", "hazelnuts"], \
         "Put the desired amount of oat in a bowl. Pour the milk \
 above. Put in the microwave for 1.30 minutes. Add some honey and \
 hazenuts. Your porridge is now ready to be eaten.", "dessert")
+tomatoes_salad = Recipe("tomatoes salad", 1, 5, ["tomatoes", "vinegar", "oil",\
+"mustard"], "Take three tomatoes and cut them vertically in slices. Take a bowl. \
+Put two spoons of oil and one spoon of vinegar. Add a little bit of mustard.\
+Pour the sauce on your tomatoes. Enjoy your tomatoes salad", "starter")
+boiled_eggs = Recipe("boiled eggs", 2, 5, ["eggs", "mayonaise"], \
+"Take two eggs and put them for four minutes in boiling water \
+Peel away the shell. Cut them in half and add mayonaise on the top\
+Enjoy your boiled eggs", "starter")
 to_print = str(porridge)
 print(to_print)
 
 first_book = Book("Easy to do")
 
-first_book.get_recipe_by_name(porridge)
+first_book.add_recipe(porridge)
+first_book.add_recipe(tomatoes_salad)
+first_book.add_recipe(boiled_eggs)
+first_book.get_recipes_by_types("starter")
+#first_book.get_recipe_by_name("tomatoes salad")
 
 # Test error cases
 
